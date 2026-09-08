eda61aaac0935b0744cd2a85fff79cbed891d945
shafiaakhter17-maker <shafiaakhter17@gmail.com>
2026-09-08 22:59:35 +0530

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "my-little-notes-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function (cacheName) {
+                        return cacheName !== CACHE_NAME;
+                    })
+                    .map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request)
+                .then(function (response) {
+                    return response;
+                })
+                .catch(function () {
+                    return caches.match("./index.html");
+                });
+
+        })
+    );
+});
